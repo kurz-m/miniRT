fa89dd13db729ccf492b3d0c2f681c73513efede
flauer <flauer@student.42heilbronn.de>
2023-10-31 14:58:46 +0100

    define structs

--- structs.h
+++ structs.h
@@ -2,33 +2,68 @@
 # define STRUCTS_H
 
 # include "colors.h"
+# include "vec3d.h"
+# include "libft.h"
 
 typedef struct s_amb
 {
-	int		type;
-	double	ratio;
-	t_color	color;
+	double		ratio;
+	t_color		color;
 }	t_amb;
 
 typedef struct	s_cam
 {
-	int		type;
-	t_vec3d	pov;
-	t_vec3d	orientation;
-	uint8_t	fov;
+	t_point3d	pov;
+	t_vec3d		dir;
+	uint8_t		fov;
 }	t_cam;
+
+typedef struct	s_obj
+{
+	int			type;
+}	t_obj;
 
 typedef struct	s_light
 {
-	int		type;
-	t_vec3d	pos;
-	double	brightness;
-	t_color	color;
+	int			type;
+	t_point3d	pos;
+	double		brightness;
+	t_color		color;
 }	t_light;
 
+typedef struct	s_sphere
+{
+	int			type;
+	t_point3d	pos;
+	double		diameter;
+	t_color		color;
+}	t_sphere;
 
-typedef struct	s_minirt {
+typedef struct	s_plane
+{
+	int			type;
+	t_point3d	pos;
+	t_vec3d		dir;
+	t_color		color;
+}	t_plane;
 
-}	t_minirt;
+
+typedef struct	s_cylinder
+{
+	int			type;
+	t_point3d	pos;
+	t_vec3d		dir;
+	double		diam;
+	double		height;
+	t_color		color;
+}	t_cylinder;
+
+typedef struct	s_scene
+{
+	t_amb	amb;
+	t_cam	cam;
+	t_list	*lights;
+	t_list	*objects;
+}	t_scene;
 
 #endif
