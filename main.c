679cc40d7776e4ac4f67c85c10149255c1ede9b4
makurz <makurz@student.42heilbronn.de>
2023-11-07 17:56:35 +0100

    remove image movement from main source file

--- main.c
+++ main.c
@@ -27,14 +27,6 @@ void ft_hook(void* param)
 
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
-	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		image->instances[0].y -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		image->instances[0].y += 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		image->instances[0].x -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		image->instances[0].x += 5;
 }
 
 // -----------------------------------------------------------------------------
