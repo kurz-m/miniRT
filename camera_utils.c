68d988fcab5a075641d8ae7257762403e1faeb6c
flauer <flauer@student.42heilbronn.de>
2023-11-14 18:32:06 +0100

    remove random

--- camera_utils.c
+++ camera_utils.c
@@ -4,11 +4,11 @@
 #include "vec3d.h"
 #include "miniRT.h"
 
-static double random_double()
-{
-	// Returns a random real in [0,1).
-	return rand() / (RAND_MAX + 1.0);
-}
+// static double random_double()
+// {
+// 	// Returns a random real in [0,1).
+// 	return rand() / (RAND_MAX + 1.0);
+// }
 
 int	get_focal_length(uint8_t fov)
 {
@@ -24,15 +24,15 @@ t_vec3d	get_pixel_center(t_cam *cam, int i, int j)
 	return (ret);
 }
 
-t_vec3d	get_pixel_random(t_cam *cam, int i, int j)
-{
-	t_vec3d	ret;
-	double	ir;
-	double	jr;
+// t_vec3d	get_pixel_random(t_cam *cam, int i, int j)
+// {
+// 	t_vec3d	ret;
+// 	double	ir;
+// 	double	jr;
 
-	ir = -0.5 + random_double() + i;
-	jr = -0.5 + random_double() + j;
-	ret = vec_add(vec_scale(cam->delta_u, ir), vec_scale(cam->delta_v, jr));
-	ret = vec_add(cam->pixel_ul, ret);
-	return (ret);
-}
+// 	ir = -0.5 + random_double() + i;
+// 	jr = -0.5 + random_double() + j;
+// 	ret = vec_add(vec_scale(cam->delta_u, ir), vec_scale(cam->delta_v, jr));
+// 	ret = vec_add(cam->pixel_ul, ret);
+// 	return (ret);
+// }
